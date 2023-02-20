51d5a7270a658e80fac2cfeddf88ed58b69e1e09
Vitaly <vitalik.emperor@gmail.com>
2023-02-21 00:03:58 +0500

    Added exercise_2

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-name = input('Введите ваше имя: ')
-password = input('Введите ваш пароль: ')
-age = input('Введите ваш возраст: ')
-print('Ваши данные для входа в аккаунт: ', 'Имя: ' + name + ',',
-      'Пароль: ' + password + ',', 'Возраст: ' + age)
+# name = input('Введите ваше имя: ')
+# password = input('Введите ваш пароль: ')
+# age = input('Введите ваш возраст: ')
+# print('Ваши данные для входа в аккаунт: ', 'Имя: ' + name + ',',
+#       'Пароль: ' + password + ',', 'Возраст: ' + age)
+
+# 2
+time = int(input("Введите время в секундах: "))
+hours = time // 3600
+minutes = (time - hours * 3600) // 60
+seconds = time - (hours * 3600 + minutes * 60)
+print(f"Время в формате чч:мм:сс   {hours} : {minutes} : {seconds}")
