394cb61f07843c8b296e9d152dfec360c0a98c36
Vitaly <vitalik.emperor@gmail.com>
2023-03-02 23:51:09 +0500

    Added lesson_3

--- lesson_3/task_3.py
+++ lesson_3/task_3.py
@@ -8,24 +8,15 @@
 пользователя.
 """
 
-goods = []
-while input("Would you like add product? Enter yes/no: ") == 'yes':
-    number = int(input("Enter product number: "))
-    features = {}
-    while input(
-            "Would you like add product parameters? Enter yes/no: ") == 'yes':
-        feature_key = input("Enter feature product: ")
-        feature_value = input("Enter feature value product: ")
-        features[feature_key] = feature_value
-    goods.append(tuple([number, features]))
-print(goods)
-# goods = [(1, {'name': 'comp', 'price': '11'}),
-# (2, {'name': 'pri', 'price': '22'})]
-analitics = {}
-for good in goods:
-    for feature_key, feature_value in good[1].items():
-        if feature_key in analitics:
-            analitics[feature_key].append(feature_value)
+products = [
+    (1, {'name': 'computer', 'price': 20000, 'amount': 5}),
+    (2, {'name': 'printer', 'price': 6000, 'amount': 2}),
+    (3, {'name': 'scanner', 'price': 2000, 'amount': 7})]
+d = {}
+for product in products:
+    for key, value in product[1].items():
+        if key in d:
+            d[key] = d[key] + [value]
         else:
-            analitics[feature_key] = [feature_value]
-print(analitics)
+            d[key] = [value]
+print(d)
